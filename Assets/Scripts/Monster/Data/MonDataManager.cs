c114582955666f8f43be9a1c0ab070fd5ead0547
only1hay <only1hay@nate.com>
2024-03-11 12:29:24 +0900

    [feat] monManager

---
+++ Assets/Scripts/Monster/Data/MonDataManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MonDataManager : MonoBehaviour
+{
+    //ΩÃ±€≈Ê
+    public static MonDataManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
