6a1b318446956fe2e7626c2e7d92c902884c4491
seohyun <leeseohyun61@naver.com>
2024-03-11 15:13:25 +0900

    feat : 아이템 상호작용

---
+++ Assets/Scripts/Inventory/ItemObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public ItemData itemData;
+
+}
---
+++ Assets/Scripts/Inventory/PickupController.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    [SerializeField] private float _range;
+    private bool _pickupActivated = false;
+    private RaycastHit hit;
+
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private TextMeshProUGUI _pickupText;
+
+    private void Update()
+    {
+        CheckItem();
+        TryAction();
+    }
+
+    private void TryAction()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            CheckItem();
+            PickUp();
+        }
+    }
+
+    // 아이템 레이어마스크 검사
+    private void CheckItem()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _range, _layerMask))
+        {
+            ItemInfoAppear();
+        }
+        else
+            ItemInfoDisappear();
+    }
+
+
+    private void ItemInfoAppear()
+    {
+        _pickupActivated = true;
+        _pickupText.gameObject.SetActive(true);
+        _pickupText.text = hit.transform.GetComponent<ItemObject>().itemData.name + " 획득하기" + " [E]";
+    }
+
+    private void ItemInfoDisappear()
+    {
+        _pickupActivated = false;
+        _pickupText.gameObject.SetActive(false);
+    }
+
+    private void PickUp()
+    {
+        if (_pickupActivated)
+        {
+            if (hit.transform != null)
+            {
+                Destroy(hit.transform.gameObject);
+                ItemInfoDisappear();
+            }
+        }
+    }
+}
