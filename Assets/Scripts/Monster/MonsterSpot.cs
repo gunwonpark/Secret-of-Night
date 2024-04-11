278659eb5b2edd43792f9384d64db0170eca9c78
only1hay <only1hay@nate.com>
2024-04-12 01:40:26 +0900

    [fix] 스폰 코드 수정

--- Assets/Scripts/Monster/MonsterSpot.cs
+++ Assets/Scripts/Monster/MonsterSpot.cs
@@ -6,6 +6,10 @@ public class MonsterSpot : MonoBehaviour
 {
     public MonsterManager manager;
     public SpawnData spawnData;
+    public MonsterInfo monsterInfo;
+
+    int monsterNumber;
+    int maxMonsterNumber = 10;
 
     public void Initialize(MonsterManager manager, SpawnData spawnData)
     {
@@ -13,6 +17,38 @@ public class MonsterSpot : MonoBehaviour
         this.spawnData = spawnData;
 
         transform.position = spawnData.spotVector;
+    }
+
+    private void Start()
+    {
+        //SpawnAllMonster();
+    }
+
+    public void SpawnAllMonster()
+    {
+        if (monsterNumber <= maxMonsterNumber)
+        {
+            for (int i = 0; i <= spawnData.MonsterID.Length; i++)
+            {
+                int ID = spawnData.MonsterID[i];
+
+                monsterInfo = manager.monsterData.monsterDatabase.GetMonsterInfoByKey(ID);//몬스터 정보 불러오기
+                Vector3 spawnPoint = GetRandomPointInCircle(spawnData.spotVector, spawnData.Radius);
+                spawnPoint.y = spawnData.spotVector.y;
+                GameObject go = Instantiate(monsterInfo.prefab, spawnPoint, Quaternion.identity);
+                monsterNumber++;
+
+                FieldMonsters fieldMonster = go.GetComponent<FieldMonsters>();
+                fieldMonster.SetPosition(spawnPoint);//포지션 전달
+                fieldMonster.Init(monsterInfo);
+
+                fieldMonster.circlePosition = transform.position;
+                fieldMonster.circleRadius = spawnData.Radius;
+
+                Debug.Log(monsterNumber);
+                Debug.Log(spawnPoint);
+            }
+        }
     }
 
     public Vector3 GetRandomPointInCircle(Vector3 center, float radius)//랜덤한 스폰지점
