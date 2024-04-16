b98e99d0eada517ce140a734439aea632503555f
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-04-16 19:30:52 +0900

    9차 병합

--- Assets/Scripts/Quest/Quest13Director.cs
+++ Assets/Scripts/Quest/Quest13Director.cs
@@ -6,8 +6,9 @@ using UnityEngine.AI;
 public class Quest13Director : MonoBehaviour
 {
     NavMeshAgent agent;
-    public Transform targetPos;
-    public GameObject questSkunk;
+    public Transform targetPos;       
+    private MonsterInfo monsterInfo;
+    private int maxCount = 1;
 
     private void Start()
     {
@@ -21,7 +22,23 @@ public class Quest13Director : MonoBehaviour
             if (DialogueHandler.I.dialogueIndex == 2)
             {
                 agent.SetDestination(targetPos.position);
+                //for (int i = 0; i < maxCount; i++)
+                //{
+                //    SpawnMonster();
+                //    maxCount--;
+                //}
+                
             }
         }          
     }
+
+    //public void SpawnMonster()
+    //{
+    //    monsterInfo = GameManager.Instance.monsterManager.monsterData.monsterDatabase.GetMonsterInfoByKey(10);
+    //    GameObject go = Instantiate(monsterInfo.prefab, new Vector3(-61.29049f, 1.010074f, 81.39434f), Quaternion.identity);
+
+    //    FieldMonsters fieldMonsters = go.GetComponent<FieldMonsters>();
+
+    //    fieldMonsters.Init(monsterInfo);
+    //}
 }
