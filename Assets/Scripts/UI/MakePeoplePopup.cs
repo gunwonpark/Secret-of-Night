9990d8f36ad1e9c5f2f40680c8aa5844b35e8c1c
gunwonpark <parkgunwon1@naver.com>
2024-04-18 14:54:28 +0900

    [fix] 만든이 UI수정

--- Assets/Scripts/UI/MakePeoplePopup.cs
+++ Assets/Scripts/UI/MakePeoplePopup.cs
@@ -1,24 +1,36 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MakePeoplePopup : UIBase
 {
     [SerializeField] private Image _background;
-    bool _isEnd = false;
+    [SerializeField] private TextMeshProUGUI _name;
+    [SerializeField] private float _time = 2f;
+    [SerializeField] private float _speed = 300f;
+    private bool _isEnd = false;
     private void Awake()
     {
         _background.rectTransform.localPosition = new Vector3(0, -1920 + transform.position.y, 0);
+        _isEnd = false;
     }
+
     void Update()
     {
         if (_isEnd == false)
         {
-            _background.rectTransform.localPosition += new Vector3(0, 1000 * Time.deltaTime, 0);
+            _background.rectTransform.localPosition += new Vector3(0, _speed * Time.deltaTime, 0);
             if (_background.transform.localPosition.y >= 850f + transform.position.y)
             {
-                // Stop scrolling or reset position
                 _isEnd = true;
+                FadeOut(_time);
             }
         }
+    }
+    void FadeOut(float time)
+    {
+        _background.CrossFadeAlpha(0, time, false);
+        _name.CrossFadeAlpha(0, time, false);
+        Destroy(gameObject, time);
     }
 }
