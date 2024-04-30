e418b2f46da1d591f055a1aa359cf06fc55ec3c5
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-05-01 06:55:45 +0900

    [fix] 타임라인 널값 뜨는 거 수정

--- Assets/Scripts/Timeline/TimelineCallbackEnd.cs
+++ Assets/Scripts/Timeline/TimelineCallbackEnd.cs
@@ -4,10 +4,9 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class TimelineCallbackBoss : MonoBehaviour
+public class TimelineCallbackEnd : MonoBehaviour
 {
-    private PlayableDirector playableDirector;
-    [SerializeField] private GameObject Boss;
+    private PlayableDirector playableDirector;    
 
     private void Start()
     {
@@ -19,9 +18,16 @@ public class TimelineCallbackBoss : MonoBehaviour
     // 타임라인 종료 시 호출되는 콜백 함수
     private void OnTimelineStopped(PlayableDirector director)
     {
-        Debug.Log("타임라인이 종료되었습니다.");
-        Boss.SetActive(true);
-       
+        Debug.Log("타임라인이 종료되었습니다.");        
+        GameManager.Instance.inputManager.EnablePlayerAction();
+        GameManager.Instance.uiManager.ShowPopupUI<MakePeoplePopup>();
+        StartCoroutine("GameEnd");
         // 타임라인 종료 시 필요한 작업 수행
-    }   
+    }
+
+    IEnumerator GameEnd()
+    {
+        yield return new WaitForSeconds(9f);
+        GameManager.Instance.sceneManager.LoadSceneAsync(Scene.Main);
+    }
 }
