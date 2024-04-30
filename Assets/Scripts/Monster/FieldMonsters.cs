f419dcc3593a60e0539236335e2a651234e3a949
only1hay <only1hay@nate.com>
2024-04-30 19:41:47 +0900

    [fix] 몬스터12~16 컬러값예외처리

--- Assets/Scripts/Monster/FieldMonsters.cs
+++ Assets/Scripts/Monster/FieldMonsters.cs
@@ -60,10 +60,17 @@ public class FieldMonsters : MonoBehaviour, IDamageable
         stateMachine.ChangeState(stateMachine.IdleState);
 
         //원래 컬러 저장
-        originalColor = new Color[meshRenderers.Length];
-        for (int i = 0; i < meshRenderers.Length; i++)
+        if (myInfo.MonsterID == 12 || myInfo.MonsterID == 13 || myInfo.MonsterID == 14 || myInfo.MonsterID == 15 || myInfo.MonsterID == 16)
         {
-            originalColor[i] = meshRenderers[i].material.color;
+            return;
+        }
+        else
+        {
+            originalColor = new Color[meshRenderers.Length];
+            for (int i = 0; i < meshRenderers.Length; i++)
+            {
+                originalColor[i] = meshRenderers[i].material.color;
+            }
         }
     }
 
@@ -219,17 +226,40 @@ public class FieldMonsters : MonoBehaviour, IDamageable
 
     private void DamageColor()
     {
+        //if (myInfo.MonsterID == 12 || myInfo.MonsterID == 13 || myInfo.MonsterID == 14 || myInfo.MonsterID == 15 || myInfo.MonsterID == 16)
+        //{
+        //    foreach (SkinnedMeshRenderer renderer in meshRenderers)
+        //    {
+        //        renderer.material.SetColor("_MainTex", damageColor);
+        //        //renderer.material.color = damageColor;
+        //    }
+        //}
+        //else
+        //{
+
         foreach (SkinnedMeshRenderer renderer in meshRenderers)
         {
             renderer.material.SetColor("_BaseColor", damageColor);
         }
+        //}
     }
 
     private void ResetColor()//원래 색상
     {
+        //if (myInfo.MonsterID == 12 || myInfo.MonsterID == 13 || myInfo.MonsterID == 14 || myInfo.MonsterID == 15 || myInfo.MonsterID == 16)
+        //{
+        //    for (int i = 0; i < meshRenderers.Length; i++)
+        //    {
+        //        meshRenderers[i].material.SetColor("_MainTex", originalColor[i]);
+        //    }
+        //}
+        //else
+        //{
+
         for (int i = 0; i < meshRenderers.Length; i++)
         {
             meshRenderers[i].material.SetColor("_BaseColor", originalColor[i]);
         }
+        //}
     }
 }
--- Assets/Scripts/Monster/MonsterData.cs
+++ Assets/Scripts/Monster/MonsterData.cs
@@ -9,7 +9,7 @@ public class MonsterInfo
     public string Name;
     public int Level;
     public int Exp;
-    public int ShortDistance;
+    public bool ShortDistance;
     public bool AtkStance;
     public float HP;
     public float Damage;
